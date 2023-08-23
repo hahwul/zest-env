2a8a1da4d0d7406c28d18f934c6c1ae0e28cf962
hahwul <hahwul@gmail.com>
2023-08-24 00:52:21 +0900

    Support amd64 and arm64 build script

--- geckodriver.sh
+++ geckodriver.sh
@@ -1,5 +1,17 @@
 #/bin/bash -l
 
-url=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[].browser_download_url | select(contains("linux64") and endswith("gz"))')
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+    url=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[].browser_download_url | select(contains("linux64") and endswith("gz"))')
+elif [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64"
+    url=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[].browser_download_url | select(contains("linux-aarch64") and endswith("gz"))')
+else
+    echo "Unsupported architecture"
+    echo "Use to x86_64 mode"
+    url=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[].browser_download_url | select(contains("linux64") and endswith("gz"))')
+fi
+
 curl -s -L "$url" -o /build/geckodriver.gz 
 tar -xvf /build/geckodriver.gz
